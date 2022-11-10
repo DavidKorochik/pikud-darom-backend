97dd55d1896203289d4ddce6340f3e1e18193b3e
David Korochik <david1korochik@gmail.com>
2022-11-10 12:46:33 +0200

    Established connection go the golang server

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+
+	router := gin.New()
+	router.Use(gin.Recovery())
+
+	port := os.Getenv("PORT")
+
+	router.Run(":" + port)
+}
