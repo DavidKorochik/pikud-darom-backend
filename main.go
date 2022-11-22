fcd1799761b0d67add6e4437a850c32a03f5633e
David Korochik <david1korochik@gmail.com>
2022-11-22 19:28:18 +0200

    Installed swagger deps and added the router for the docs

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/DavidKorochik/pikud-darom-backend/initializers"
 	"github.com/DavidKorochik/pikud-darom-backend/routes"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func init() {
@@ -17,12 +19,13 @@ func main() {
 	db.DBConnection()
 
 	router := gin.Default()
+	port := os.Getenv("PORT")
 
 	routes.IssueRoutes(router)
 	routes.UserRoutes(router)
 	routes.AuthRoutes(router)
 
-	port := os.Getenv("PORT")
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	router.Run(port)
 }
